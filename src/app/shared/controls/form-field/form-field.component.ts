64bfac251d0e846507817f54bb297e8fc4e06627
Micky <97794430+flasruberum@users.noreply.github.com>
2023-06-19 14:36:42 -0400

    form-field parte 1

--- src/app/shared/controls/form-field/form-field.component.ts
+++ src/app/shared/controls/form-field/form-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form-field',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-field.component.scss']
 })
 export class FormFieldComponent implements OnInit {
+  @Input() label!: string;
 
   constructor() { }
 
